e960d76da2676668fe620fb2370590fafed62072
Nick H <nhuntington11@gmail.com>
2018-06-20 10:56:55 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,23 @@
+function theBeatlesPlay(musicians, instruments) {
+  var finishedList = []
+  
+  for (var i = 0; i>-1; i++) {
+    if (musicians[i] === undefined) {
+      return finishedList
+    } else {
+      finishedList[i] = musicians[i] + ' plays ' + instruments[i]
+    }
+  }
+}
+
+function johnLennonFacts(facts) {
+  var i = 0
+  var list = []
+  
+  while (facts[i] !== undefined) {
+    list[i] = facts[i] + "!!!"
+    i += 1
+  }
+  
+  return list
+}
